ceab9bd4f2a5e6f439f43f8bd306a3c3c685fc80
fortredux <fortunaredux@protonmail.com>
2020-03-08 20:01:59 +0000

    Added new version of recursion_ver_2

--- grok/recursion_ver_2.py
+++ grok/recursion_ver_2.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+"""
 n = input('Enter number in range from 1 to 9: ')
 
 
@@ -6,17 +9,46 @@ def countdown(i):
         i = int(i)
     except ValueError:
         return print('You should have entered number')
-        
+
     if i == 0:
         return print(0)
         
     if i > 9:
         return print('Number must be less than 10')
-        
+
     if i > 0:
         print(i)
+        sleep(1)
         return countdown(i-1)
-        
+
+    else:
+        return print('Number must be positive')
+"""
+
+n = input('Enter number in range from 1 to 15: ')
+
+
+def countdown(i):
+    check = False
+
+    while check == False:
+        try:
+            i = int(i)
+            if i == 0:
+                return print(0)
+            elif i > 15:
+                i = input('Number must be less than 16. Try again: ')
+            elif i < 16:
+                check = True
+
+        except ValueError:
+            i = input('You should have entered number in range from 1 to 15: ')
+
+    if i > 0:
+        print(i)
+        sleep(0.1)
+        return countdown(i-1)
+
     else:
         return print('Number must be positive')
 
